3ec5de00d037fb0039f3e7aa07db546cab120b71
Ewen Cluley <ewencluley@gmail.com>
2020-10-11 14:24:41 +0100

    add multiple stations and random choice when alarm triggers

--- Radio.py
+++ Radio.py
@@ -1,9 +1,36 @@
+import json
 import subprocess
+from collections import namedtuple
+from json.decoder import JSONDecodeError
+import random
 
-URL = 'http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/uk/sbr_high/llnw/bbc_6music.m3u8'
+Station = namedtuple("Station", "name url")
+stations = {}
+
+
+def load_stations():
+    try:
+        with open("stations.json", "r") as f:
+            for s in json.loads(f.read()):
+                station = Station(**s)
+                stations[station.url] = station
+    except FileNotFoundError or TypeError or JSONDecodeError:
+        default_station = Station(name='BBC 6 Music',
+                                  url='http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/uk/sbr_high/llnw/bbc_6music.m3u8')
+        stations[default_station.url] = default_station
+    print(stations)
+
+
+load_stations()
+
 
 class RadioState:
     triggered_by_alarm = False
+    current_url = subprocess.check_output(f' mpc -f "%file%" playlist', shell=True, stderr=subprocess.STDOUT).decode("utf-8").strip()
+    if current_url:
+        current_station = stations[current_url]
+    else:
+        current_station = list(stations.values())[0]
 
 
 state = RadioState()
@@ -19,8 +46,10 @@ def is_playing() -> bool:
 
 def play(triggered_by_alarm=False):
     try:
+        if triggered_by_alarm:
+            state.current_station = list(stations.values())[random.randrange(start=0, stop=len(stations))]
         state.triggered_by_alarm = triggered_by_alarm
-        subprocess.check_output(f'mpc clear && mpc add {URL} && mpc play', shell=True, stderr=subprocess.STDOUT)
+        subprocess.check_output(f'mpc clear && mpc add {state.current_station.url} && mpc play', shell=True, stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError:
         print("Something went wrong while playing radio")
 
